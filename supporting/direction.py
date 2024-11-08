ea5b55d4fe1ea894b859111e19ae45947806ef0a
peer3737 <petervanelsacker@gmail.com>
2024-11-08 20:10:02 +0100

    weather table changed to weather_meteo

--- supporting/direction.py
+++ supporting/direction.py
@@ -6,7 +6,7 @@ from database.db import Connection
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(os.environ.get("LOG_LEVEL", logging.INFO))
-
+weather_table = 'weather_meteo'
 
 def calculate_bearing(lat1, lon1, lat2, lon2, wind_direction):
     colors = {
@@ -93,7 +93,7 @@ def execute():
         act_counter += 1
         log.info(f'Handling activity {activity_id} ({act_counter}/{total_act})')
         stream = db.get_specific(table="activity_streams", where=f"activity_id = {activity_id}")
-        weather = db.get_specific(table="weather_knmi", where=f"activity_id = {activity_id}")
+        weather = db.get_specific(table=weather_table, where=f"activity_id = {activity_id}")
         wind = weather[0][4].split(', ')
 
         activity_latlngs = stream[0][5].split('],[')
